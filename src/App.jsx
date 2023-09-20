c58941dc594d927f1e557ffd5d132e0385283039
Ebru <yerdebru@gmail.com>
2023-09-20 23:56:35 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 import Header from "./Header.jsx";
 import MainContent from "./MainContent.jsx";
 
-//an ex of composing a two components
-//Page is the parent component and Header,Footer,MainContent are child components
 function App() {
   return (
     <>
