1386aa1bac031378a5a19054bbc3a84ed1354731
Ebru <yerdebru@gmail.com>
2023-09-21 19:29:22 +0300

    changes of images

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header>
       <nav className="nav">
         <div className="nav-bar-left">
-          <img src="src/assets/images/react-logo.png" />
+          <img src="/react-logo.png" />
           <h1 className="ReactFacts">ReactFacts</h1>
         </div>
         <h3 className="ReactCourseText">React Course - Project 1</h3>
